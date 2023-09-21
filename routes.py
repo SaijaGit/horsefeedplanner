e33bb6cefcc12cc6065eeb17ab2bf176212d0fd0
SaijaGit <saija.piira@gmail.com>
2023-09-22 02:01:04 +0300

    Created a Flask project and implemented login functionality.

---
+++ routes.py
@@ -0,0 +1,44 @@
+from app import app
+from flask import render_template, request, session, redirect, flash
+import users
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            flash("Incorrect username or password!", "error")
+            return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            flash("Passwords don't match!", "error")
+            return render_template("register.html")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            flash("Account creation failed:(", "error")
+            return render_template("register.html")
